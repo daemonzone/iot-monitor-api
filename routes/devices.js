6d81beeae1ab64157f25714f94034f8541a01555
Davide Villani <daemonzone@users.noreply.github.com>
2025-11-07 23:01:01 +0100

    Devices also include last sensor data reading (temp/hum)

--- routes/devices.js
+++ routes/devices.js
@@ -7,8 +7,38 @@ const router = express.Router();
 // List all devices
 router.get("/", authenticateToken, async (req, res) => {
   try {
-    const result = await pool.query("SELECT * FROM devices ORDER BY id");
-    res.json(result.rows);
+    const results = await pool.query(`
+      SELECT d.*,
+             r.id as reading_id,
+             r.recorded_at,
+             r.temperature,
+             r.humidity
+      FROM devices d
+      LEFT JOIN LATERAL (
+          SELECT id, recorded_at, temperature, humidity
+          FROM devices_readings
+          WHERE device_id = d.device_id
+            AND (temperature IS NOT NULL OR humidity IS NOT NULL)
+          ORDER BY recorded_at DESC
+          LIMIT 1
+      ) r ON true
+      ORDER BY d.id
+    `);
+
+    const devices = [];
+
+    for (const device of results.rows) {
+      device.readings = [{
+        id: parseInt(device.reading_id),
+        recorded_at: device.recorded_at.toISOString(),
+        temperature: parseFloat(device.temperature),
+        humidity: parseFloat(device.humidity)
+      }];
+      
+      devices.push(device); // <-- important!
+    }
+
+    res.json(devices);
   } catch (err) {
     console.error(err);
     res.status(404).json({ error: "Not found" });
@@ -25,6 +55,7 @@ router.get("/:id", authenticateToken, async (req, res) => {
           device_id,
           model,
           ip_addr,
+          image,
           uptime,
           status,
           last_status_update,
